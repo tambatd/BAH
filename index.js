c3888c413522426b2ce6bf58e48d0e99390af407
Devendra Tambat <tambatd@outlook.com>
2020-12-28 03:20:07 -0500

    olivia

--- index.js
+++ index.js
@@ -9,20 +9,18 @@ client.on('ready', () => {
   const channel = client.channels.cache.get("734942504927100932");
   channel.join().then(connection => {
     console.log("Successfully connected.");
-    connection.play('audio.mp3', { volume: 0.5 });
-
+    client.on('message', msg => {
+      if (msg.content === "olivia going bah") {
+        msg.reply("BAAAAAAAAAAAAAAH ");
+        connection.play('audio.mp3', { volume: 0.5 });
+      }
+    });
   }).catch(e => {
     console.error(e);
   });
   });
   
-  client.on('message', msg => {
-    if (msg.content === "olivia going bah") {
-      msg.reply("BAAAAAAAAAAAAAAH ");
-      //connection.play('audio.mp3', { volume: 0.5 });
-
-    }
-  });
+  
   
 
 console.log("idk if it started lol")
