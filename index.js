1e3e41aa9e27306237b17bda8d07aa0c19ee42a5
deve <tambatd@outlook.com>
2020-12-29 15:29:44 -0500

    *

--- index.js
+++ index.js
@@ -1,27 +1,63 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
+require('dotenv').config()
+//const config = require("./config.json");
 var CronJob = require('cron').CronJob;
 const fs = require('fs');
 const client = new Discord.Client();
 
-
+/*
 client.on('ready', () => {
   const channel = client.channels.cache.get("734942504927100932");
   channel.join().then(connection => {
     console.log("Successfully connected.");
     client.on('message', msg => {
-      if (msg.content === "olivia going bah") {
-        msg.reply("BAAAAAAAAAAAAAAH ");
-        connection.play('audio.mp3', { volume: 0.5 });
+      if (msg.content === "Olivia going bah") {
+        msg.reply("BAAAAAAAAAAAAAAH");
+        connection.play('olivia_bah.mp3', { volume: 0.5 });
       }
+      else if (msg.content === "Mike going nonono") {
+        msg.reply("NONONONONONOOOOO");
+        connection.play('mike_no.mp3', { volume: 0.5 });
+      }
+      else if (msg.content === "BAH BOT stop;") {
+        msg.reply("Service Stop");
+
+        connection.destroy()
+        //client.destroy();
+      } 
     });
   }).catch(e => {
     console.error(e);
   });
   });
-  
-  
-  
+  */
 
+  client.on('message', msg => {
+    if (msg.content === "BAH BOT start;") {
+      const channel = client.channels.cache.get("734942504927100932");
+      channel.join().then(connection => {
+        console.log("Successfully connected.");
+        client.on('message', msg => {
+          if (msg.content === "Olivia going bah") {
+            msg.reply("BAAAAAAAAAAAAAAH");
+            connection.play('olivia_bah.mp3', { volume: 0.5 });
+          }
+          else if (msg.content === "Mike going nonono") {
+            msg.reply("NONONONONONOOOOO");
+            connection.play('mike_no.mp3', { volume: 0.5 });
+          }
+          else if (msg.content === "BAH BOT stop;") {
+            msg.reply("Service Stop");
+            channel.leave();
+          } 
+        });
+      }).catch(e => {
+        console.error(e);
+      });
+      msg.reply("connected to vc");
+    } 
+  });
+ 
+
+client.login(process.env.TOKEN);
 console.log("idk if it started lol")
-client.login(config.BOT_TOKEN);
